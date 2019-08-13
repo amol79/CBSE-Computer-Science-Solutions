d27742a23ef22626125a44ff0617716a33f2ac8a
amol79 <32371272+amol79@users.noreply.github.com>
2019-08-13 22:38:07 +0530

    Update twelve_u1_ch3_pg47.py

--- Class12/twelve_u1_ch3_pg47.py
+++ Class12/twelve_u1_ch3_pg47.py
@@ -1,25 +1,27 @@
 class Mobile:
   '"A sample class definition"'
-  price = 0
-  model = "Null"
+  price = 0 #class variable
+  model = "Null" #class variable
   def __init__(self, price, model = None):
-    self.price=price
-    self.model=model
-    #Mobile.model = model #will also give same result
+    self.price=price #object variables
+    self.model=model 
   
-  def displaydata(self, ):
-    print("Details of Mobile are: ", self. price, self. model)
-    #print("Details of Mobile are: ", self. price, Mobile. model) #will give same result
+  def displayData(self, ):
+    print("Details of Mobile are: ", self. price, self. model) #object variables
+
+  def displayClassData():
+    print("Details of class variables are:", Mobile.price, Mobile.model) #class variables
 
 M1 = Mobile(10000, "Redmi Note 3")
-M1.displaydata()
-
 M2 = Mobile(15000, "Realme 2 Pro")
-M2.displaydata()
+M1.displayData()
+M2.displayData()
+Mobile.displayClassData()
 
 """
 Python 3.7.4 (default, Jul  9 2019, 00:06:43)
 [GCC 6.3.0 20170516] on linux
 Details of Mobile are:  10000 Redmi Note 3
 Details of Mobile are:  15000 Realme 2 Pro
+Details of class variables are: 0 Null
 """
